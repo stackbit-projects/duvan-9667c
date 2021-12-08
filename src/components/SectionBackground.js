9d8497b42c2e5ee565c5b80012731f41fa55e492
Stackbit Code Editor <projects@stackbit.com>
2021-12-08 19:22:14 +0000

    SectionBackground.js: changed

--- src/components/SectionBackground.js
+++ src/components/SectionBackground.js
@@ -21,8 +21,8 @@ export default class SectionBackground extends React.Component {
         //            backgroundSize: backgroundImageSize,
         //            backgroundPosition: backgroundImagePosition,
         //            backgroundRepeat: backgroundImageRepeat
-                }}
-            />
+        //        }}
+        //    />
         );
     }
 }
